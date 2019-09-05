abbf83069fb038c0a4be93d48375e2ead7f50b8a
Liz Laffitte <liz.kossakowski@gmail.com>
2019-09-05 00:18:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,5 @@
 def find_element_index(array, value_to_find)
-  array.length.times |index|
-    if array[index] == value_to_find
-      return index 
-    end
-  end 
+  return array.index(value_to_find)
 end
 
 def find_max_value(array)
